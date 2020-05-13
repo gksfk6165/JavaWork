a42d8337198bd6e4e2159cea54ba99ab4a2dc881
gksfk6165 <gksfk308@gmail.com>
2020-05-13 17:42:04 +0900

    20200513

---
+++ Lec_WEB/WebContent/myweb/samsung.js
@@ -0,0 +1,168 @@
+$(document).ready(function () {
+    $('.single-item').slick({
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        auotoplaySpeed: 1000,
+        arrows: false
+    });
+
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true
+    });
+
+    $(".imo_div").click(function () {
+        if ($(".imo_div").hasClass("off")) {
+            $(".imo_two").animate({
+                bottom: "+=75",
+            }, 1000);
+            $(".imo_three").animate({
+                bottom: "+=150",
+            }, 1000);
+            $(".imo_four").animate({
+                bottom: "+=225",
+            }, 1000);
+            $(".imo_div").removeClass("off");
+            $(".imo_div").addClass("on");
+        } else if ($(".imo_div").hasClass("on")) {
+            $(".imo_two").animate({
+                bottom: "-=75",
+            }, 1000);
+            $(".imo_three").animate({
+                bottom: "-=150",
+            }, 1000);
+            $(".imo_four").animate({
+                bottom: "-=225",
+            }, 1000);
+            $(".imo_div").removeClass("on");
+            $(".imo_div").addClass("off");
+        }
+    });
+
+
+    $(function () {
+        $(".header_img").click(function () {
+            if ($(".login").hasClass("no_open")) {
+                $('ul:first', this).show();
+                $(".login").removeClass("no_open");
+                $(".login").addClass("open");
+            } else {
+                $('ul:first', this).hide();
+                $(".login").removeClass("open");
+                $(".login").addClass("no_open");
+            }
+
+
+        });
+    });
+
+
+    $(".register_agree").click(function () {
+
+
+        if ($("input:checkbox[name=term1]:checked").length !=
+            $("input:checkbox[name=term1]").length) {
+            alert("약관 전부 동의해주세요!");
+        } else {
+            location.href = 'createid.html';
+        }
+
+    });
+
+    $(".register_no_agree").click(function () {
+        location.href = 'login.html';
+    });
+    $('.check_all').click(function () {
+        if ($("input:checkbox[name=term2]").is(":checked") != true) {
+            $("input[name=term1]:checkbox").prop("checked", false);
+        } else {
+            $("input[name=term1]:checkbox").prop("checked", true);
+
+        }
+    });
+    
+
+
+    url = "samsung_phone.json"
+
+
+    $("input:checkbox").click(function () {
+
+        var count = $(this).val();
+
+        // if($(this).hasClass("no_check")){
+        //     check=0;  //체크할때
+        // }else{
+        //     check=1;
+        // }
+        $.ajax({
+            url: url,
+            type: "GET",
+            cache: false,
+            success: function (data, status) {
+                    checkJSON(data, count);
+
+                        $('.new_box').click(function(){
+
+                            var one = $(this).val();
+                            $.ajax({
+                                url: url,
+                                type: "GET",
+                                cache: false,
+                                success: function (data, status) {
+                                    
+                                    imgJSON(data,one,count);
+                                    
+                                }
+                        });
+                    });
+            
+        }});
+
+
+
+    });
+    
+
+
+});
+function checkJSON(xmlDOM, val) {
+    var row = xmlDOM.phone;
+    var idx = 0;
+
+    if ($('.phone_hide  div').hasClass("box" + val)) {
+        idx-=$('.phone_hide div').length;
+        $('.box' + val).remove();
+        return;
+    }
+    for (i = 0; i<row.length; i++) {
+        if (val == row[i].value1 ) {
+            $(".phone_hide").append("<div class='box" + val + " span"+idx+"'><input type='checkbox' class='new_box "+val+"' name='boxbox' value='" + row[i].cal + "'><span class='span"+idx+"'>" + row[i].name + "</span></div>");
+            idx++;
+        }
+    }
+    
+
+};
+
+function imgJSON(xmlDOM, val,two){
+    var row = xmlDOM.phone;
+
+    if ($('.phone_img_hide  div').hasClass("box" + val)) {
+        $('.phone_img.box' + val).remove();
+        return;
+    };
+
+    for(i=0;i<row.length;i++){
+        if(val == row[i].cal  && two ==row[i].value1){
+            $(".phone_img_hide").append("<div class='phone_img box"+two+"'><img src='"+row[i].image+"' style='width:200px;height:200px'/></div>");
+            return;
+        }
+    }
+}
+
+
+
